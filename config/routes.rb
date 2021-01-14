ea7f44071437985643891ce5dbee4c31f3fac99c
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-01-14 17:50:04 +0900

    #8[スタジオ一覧追加]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to:'homes#index'
   get 'studios/show'
+  get '/studios' => 'studios#index'
   get '/rooms' => 'rooms#index'
   get 'rooms/show'
   resources :events
   devise_for :studios
-  devise_for :users 
+  devise_for :users
   get 'users/show' => 'users#show'
   resources :rooms, only: %i[show create] do
     resources :messages, only: [:create]
