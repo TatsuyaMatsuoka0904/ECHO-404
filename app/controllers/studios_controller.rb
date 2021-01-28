c01efc45229ae8e12fe816955e8d3a342ed0a4d6
EC2 Default User <ec2-user@ip-172-31-13-221.ap-northeast-1.compute.internal>
2021-01-28 13:53:37 +0000

    studio controller

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,4 +7,36 @@ class StudiosController < ApplicationController
     @studio = Studio.ransack(params[:q])
     @studios = @studio.result(distinct: true)
   end
+  
+ 
+  def edit
+    @studio = Studio.find(params[:id])
+    if @studio == current_studio
+    else
+      redirect_to studio_path(current_studio)
+    end
+  end
+
+  def update
+    @studio = Studio.find(params[:id])
+    if @studio.update(studio_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to studio_path
+    else
+      render "edit"
+    end
+  end
+  protected
+  
+  def studio_params
+    params.require(:studio).permit(:name, :image_id, :introduction,:address)
+  end
+  
+  def correct_studio
+   @studio = Studio.find(params[:id])
+   if current_studio != @studio
+    redirect_to studio_path(current_studio.id)
+   end
+  end
+  
 end
